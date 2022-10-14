f41e3d62d47e4bdcfe0a6eae1bee6bfe6b468a52
Ewoud Kuijpers <74767424+Ewaudtkuijpers@users.noreply.github.com>
2022-10-14 19:26:21 +0000

    menu optie 1, 2 en 5 af

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
 import random
- 
-from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
 
-## Text menu in Python
-      
-def print_menu():       ## Your menu design here
-    print (30 * "-" , "MENU" , 30 * "-")
-    print ("1. Menu Option 1")
-    print ("2. Menu Option 2")
-    print ("3. Menu Option 3")
-    print ("4. Menu Option 4")
-    print ("5. Exit")
-    print (67 * "-")
-  
-loop=True      
-  
-while loop:          ## While loop which will keep going until loop = False
-    print_menu()    ## Displays menu
-    choice = input("Enter your choice [1-5]: ")
-     
-    if choice==1:     
-        print ("Menu 1 has been selected")
-        ## You can add your code or functions here
-    elif choice==2:
-        print ("Menu 2 has been selected")
-        ## You can add your code or functions here
-    elif choice==3:
-        print ("Menu 3 has been selected")
-        ## You can add your code or functions here
-    elif choice==4:
-        print ("Menu 4 has been selected")
-        ## You can add your code or functions here
-    elif choice==5:
-        print ("Menu 5 has been selected")
-        ## You can add your code or functions here
-        loop=False # This will make the while loop to end as not value of loop is set to False
-    else:
-        # Any integer inputs other than values 1-5 we print an error message
-        print ("Wrong option selection. Enter any key to try again..")
- 
+from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot, EvaluationAgent, start, train, save
+
+
+class MyRandomAgent(EvaluationAgent):
+
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1, 500)
+
+
 class MyAgent(MLAgent):
+
     def evaluate(self, board):
         if is_winner(board, self.symbol):
             reward = 1
@@ -48,16 +19,65 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
-    
+        my_agent = MyAgent()
+       
+
+
+def TrainenEnPlotten():
+  random.seed(1)
+
+  my_agent = MyAgent()
+  random_agent = RandomAgent()
+
+
+  train_and_plot(agent=my_agent,
+                 validation_agent=random_agent,
+                 iterations=50,
+                 trainings=100,
+                 validations=1000)
+
+
+def print_menu():  
+    print(30 * "-", "MENU", 30 * "-")
+    print("1. Player vs Player")
+    print("2. Player vs Noob bot")
+    print("3. Bot trainen")
+    print("4. Player vs Smart bot")
+    print("5. validatie grafieken plotten")
+    print(67 * "-")
+
+
+loop = True
+
+while loop:  
+    print_menu()  
+    choice = int(input("Enter your choice [1-5]: "))
+
+    if choice == 1:
+        print("Player vs Player has been selected")
+        start()
+    elif choice == 2:
+        print("Player vs Noob bot has been selected")
+        my_random_agent = MyRandomAgent()
+        start(player_o=my_random_agent, player_x=my_random_agent)
+    elif choice == 3:
+        print("Bot trainen has been selected")
+        my_agent = MyAgent()
+        train(my_agent, 3000)
  
+        save(my_agent, 'MyAgent_3000')
+    elif choice == 4:
+        print("Player vs Smart bot has been selected")
+        start(player_o= 'MyAgent_3000')
+    elif choice == 5:
+        print("validatie grafieken plotten has been selected")
+        TrainenEnPlotten()
+    else:
+        
+        print("Wrong option selection. Enter any key to try again..")
+
 random.seed(1)
- 
+
 my_agent = MyAgent()
 random_agent = RandomAgent()
- 
-train_and_plot(
-    agent=my_agent,
-    validation_agent=random_agent,
-    iterations=50,
-    trainings=100,
-    validations=1000)
+
